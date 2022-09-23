b26e2c983e8b3b06e527b8669e2d3d90abcb4018
openpowerwtf <52765606+openpowerwtf@users.noreply.ggithub.com>
2022-09-22 21:45:42 -0500

    inline asm

--- rt-test/src/main.rs
+++ rt-test/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(asm_experimental_arch)]
+
 #![no_std]
 #![no_main]
 
@@ -5,7 +7,31 @@ extern crate rt;
 
 #[no_mangle]
 pub fn main() -> ! {
-    let mut _x = 0x8675309;
-
-    loop {}
+   let mut x = 0x8675309;
+   let mut  y:u32 = 0;
+   y = wtf();
+   //x = x + y; memcpy, etc. not found???
+   loop {}
 }
+
+// works with feature and nightly
+// asm! is inline to function
+// global_asm! is in global scope, outside of function
+#[no_mangle]
+pub fn wtf() -> u32 {
+   use core::arch::asm;
+   let mut x: u32 = 0;
+   unsafe {
+      asm! (
+         "cmpwi {x},0",
+         "lis  {x},2151",
+         "ori  {x},{x},21257",
+         /* x = in(reg) x */
+         /* x = out(reg) x */
+         x = inout(reg) x => x
+      );
+   }
+   //x += 1;  memcpy, etc. not found???
+   x
+}
+
